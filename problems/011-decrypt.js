1469cd2136da0e28cfb16876317145f5bb66dbf1
Abdullaev Tokhirbek <tonyoneshelby@gmail.com>
2023-05-25 15:36:24 +0500

    done 011-decrypt task

--- problems/011-decrypt.js
+++ problems/011-decrypt.js
@@ -16,7 +16,17 @@
  * @returns {string}
  */
 function decrypt(secret) {
-    return undefined;
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    return secret.split('').map((value) => {
+        if (alphabet.includes(value)) {
+            const charIndex = alphabet.indexOf(value);
+            if (charIndex === alphabet.length - 1) {
+                return alphabet[0]
+            }
+            return alphabet[charIndex + 1];
+        }
+        return value;
+    }).join('');
 }
 
 module.exports = decrypt;
