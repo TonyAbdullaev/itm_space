d1a6da950be0628c6ecd2b10d93cfb630875147f
Abdullaev Tokhirbek <tonyoneshelby@gmail.com>
2023-05-25 17:42:32 +0500

    done 014-fibonacci task

--- problems/014-fibonacci.js
+++ problems/014-fibonacci.js
@@ -15,9 +15,12 @@
  *
  * @param {number} n >= 0
  * @returns {number}
+ *
+ *
+ *
  */
 function fibonacci(n) {
-    return undefined;
+    return n <= 1 ? n : fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 module.exports = fibonacci;
